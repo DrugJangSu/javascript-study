a62557cac384b78d1b4eb289ab6c6e7574c93d76
DrugJangSu <ljonghyun6088@gmail.com>
2025-07-26 17:37:13 +0900

    Update

--- javascript2025.js
+++ javascript2025.js
@@ -1,5 +1,10 @@
 // This file is part of the JavaScript study exercises of a Udemy Course
 // The Complete Javascript Course 2025: From Zero to Expert!
+// console.log("Hello, JavaScript!");
 
-console.log("Hello, JavaScript!");
-// Javascript Fundamentals : Part 1
+// *** Javascript Fundamentals : Part 1 ***
+
+// How to open the console in the browser(chrome);
+// CMD + Option + J (Mac)
+// CTRL + Shift + J (Windows/Linux)
+
