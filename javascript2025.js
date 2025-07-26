24e051c7bf504cbdc496459297e22df935739419
DrugJangSu <ljonghyun6088@gmail.com>
2025-07-26 20:35:16 +0900

    Update

--- javascript2025.js
+++ javascript2025.js
@@ -8,3 +8,81 @@
 // CMD + Option + J (Mac)
 // CTRL + Shift + J (Windows/Linux)
 
+// let js = "amazing";
+// if (js ==="amazing") alert("JavaScript is FUN!");
+
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 +23 - 10);
+
+// <!DOCTYPE html>
+// <html lang="en">
+//   <head>
+//     <meta charset="UTF-8" />
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+//     <title>JavaScript Fundamentals – Part 1</title>
+//     <style>
+//       body {
+//         height: 100vh;
+//         display: flex;
+//         align-items: center;
+//         background: linear-gradient(to top left, #28b487, #7dd56f);
+//       }
+//       h1 {
+//         font-family: sans-serif;
+//         font-size: 50px;
+//         line-height: 1.3;
+//         width: 100%;
+//         padding: 30px;
+//         text-align: center;
+//         color: white;
+//       }
+//     </style>
+//   </head>
+//   <body>
+//     <h1>JavaScript Fundamentals – Part 1</h1>
+
+//     <script src="script.js"></script>
+//   </body>
+// </html>
+
+// ** Values ad Variables **
+
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = "Matilda";
+// // Let is used to declare variables that can be reassigned later
+// // let first = 'jonas'
+// // let firstNamePerson
+// // let first_name_person
+// // These are examples of variable names that are used in javascript
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// // let 3years = 3; // This is not a valid variable name, it cannot start with a number 
+
+// // let jonas&matilda = 'JM'; // This is also not a vaild variable name, it cannot contain special characters like &
+
+// let jonas_matilda = 'JM'
+// // let new = 27: // this is also not vaild, 'new' is a reserved keyword in Javascript
+// let $function = 27; // this is also a reserved keyword in javascript
+// let name = "Jonas"; // This is vaild, but name is not a good variable name because it's too generic and confusing.
+// let Person = 'jonas'; // This is vaild, but it's not a good variable name because it's not a lowercase -which is a common convention in JavaScript for variable names.
+// let PI = 3.1415; // This is a good variable name because it's a real constant. (Pie is also used)
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+// // Keeping the variable names in camelCase is a good practice in JavaScript.
+// let job1 = 'Programmer';
+// let job2 = 'Teacher';
+// // As you can see, the variable names are descriptive as the former than the latter.
+
+// console.log(myFirstJob);
+
+// * Practice Assignment *
