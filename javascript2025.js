f66d02c1ea643c588465e08db7146d9f8c68ff09
DrugJangSu <ljonghyun6088@gmail.com>
2025-07-28 16:07:17 +0900

    Update

--- javascript2025.js
+++ javascript2025.js
@@ -152,4 +152,36 @@
 // lastName = 'Schmedtmann';
 // console.log(lastName); // This will log 'Schmedtmann' to the console, even though lastName was not declared with let, const, or var. This is because JavaScript allows undeclared variables, but it is not a good practice and can lead to bugs in your code. Always declare your variables with let, const, or var to avoid this issue.
 
+// ** Basic Operators **
+
+// // Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3); 
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 =8 <- 2의 3승
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName); 
+// // Concatenation of strings
+
+
+// // Asssignment operators
+// let x = 10 + 5; // which is 15
+// x += 10; // x = x + 10;, which is 25
+// x *= 4; // x = x*4; // which is 100
+// x++; // x = x + 1; // which is 101
+// x--; // x = x - 1; // which is 100
+// console.log(x); // This will log 100 to the console, which is the final value of x after all the operations.
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018)
 
