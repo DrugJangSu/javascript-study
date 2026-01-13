a0c8fb507d95eeae6291016db3f0419a651c3125
DrugJangSu <ljonghyun6088@gmail.com>
2026-01-13 16:14:42 +0900

    Update JavaScript courses

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,177 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = "Matilda";
+// // Let is used to declare variables that can be reassigned later
+// // let first = 'jonas'
+// // let firstNamePerson
+// // let first_name_person
+// // These are examples of variable names that are used in javascript
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// // Variable name conventions
+// // let 3years = 3; // This is not a valid variable name, it cannot start with a number
+
+// // let jonas&matilda = 'JM'; // This is also not a vaild variable name, it cannot contain special characters like &
+
+// let jonas_matilda = 'JM'
+// // let new = 27: // this is also not vaild, 'new' is a reserved keyword in Javascript
+// let $function = 27; // this is also a reserved keyword in javascript
+// let name = "Jonas"; // This is vaild, but name is not a good variable name because it's too generic and confusing.
+// let Person = 'jonas'; // This is vaild, but it's not a good variable name because it's not a lowercase -which is a common convention in JavaScript for variable names.
+// let PI = 3.1415; // This is a good variable name because it's a real constant. (Pie is also used)
+
+// let myFirstJob = 'Programmer';
+// let myCurrentJob = 'Teacher';
+// // Keeping the variable names in camelCase is a good practice in JavaScript.
+// let job1 = 'Programmer';
+// let job2 = 'Teacher';
+// // As you can see, the variable names are descriptive as the former than the latter.
+
+// console.log(myFirstJob);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// // This is a descriptive variable name holding a Boolean value.
+
+// // console.log(typeof true); // This will log 'boolean' to the console, indicating the type of the value.
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!'; // Reassigning the variable to a string value
+// console.log(typeof javascriptIsFun);
+// // This will log 'string' to the console, indicating the type of the value after reassignment.
+
+// let year; // Declaring a variable without a value
+// console.log(year); // This will log 'undefined' to the consol,e indicating that the variable has not been assigned a value.
+// console.log(typeof year); // This will log 'undefined' to the console, indicating the type of the value.
+
+// year = 1991; // Assigning a value to a variable
+// console.log(typeof year); // This will log the value 1991 to the console.
+// console.log(typeof null); // This will log 'object' to the console, which is a known bug in JavaScript. Null is actually a primitive value that represents the absence of any object value.
+
+// let age = 30;
+// age = 31; // Reassigning the value of the variable
+
+// const birthYear = 1991; // Declaring a constant variable(which cannot be reassigned)
+// // birthYear = 1990; // This will throw an error because constants cannot be reassigned
+
+// // const job; // This will throw an error because constants must be initialized with a value
+// // The instructor recommends using const for variables that will not be reassigned as the default option, and let for variables that will be reassigned later.
+// // let is used for variables that can change, while const is used for variables that should not change.
+
+// var job = 'programmer';
+// job = 'teacher'
+// // var is an older way to declare variables, but it has function scope and can lead to issues with hoisting. It is generally recommended to use let and const instead of var in modern JavaScript.
+// // The instructor says never use var, lol
+
+// lastName = 'Schmedtmann';
+// console.log(lastName);
+// // This will log 'Schmedtmann' to the console, even though lastName was not declared with let, const, or var. This is because JavaScript allows undeclared variables, but it is not a good practice and can lead to bugs in your code. Always declare your variables with let, const, or var to avoid this issue.
+
+// // ** Basic Operators **
+
+// // Math operators
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2 =8 <- 2의 3승
+
+// const firstName = 'Jonas';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + ' ' + lastName);
+// // Concatenation of strings
+
+// // Asssignment operators
+// let x = 10 + 5; // which is 15
+// x += 10; // x = x + 10;, which is 25
+// x *= 4; // x = x*4; // which is 100
+// x++; // x = x + 1; // which is 101
+// x--; // x = x - 1; // which is 100
+// console.log(x); // This will log 100 to the console, which is the final value of x after all the operations.
+
+// // Comparison operators
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018)
+
+// // ** Operator precedence **
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2018);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// // Average age calculation
+// console.log(ageJonas, ageSarah, averageAge);
+
+// ** Challenge #1 **
+
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+// Your task is to write some code to help them:
+
+// Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+// Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+// Log the value of BMIMark and BMIJohn to the console.
+
+// BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+// TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+// TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+// MY SOLUTION
+// const massMark = 78;
+// const massJohn = 92;
+
+// const heightMark = 1.69;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// console.log(BMIMark, BMIJohn);
+
+// Bonus(create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.)
+
+// console.log(BMIMark > BMIJohn);
+
+// instructor's solution
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// ** Strings and Template Literals **
