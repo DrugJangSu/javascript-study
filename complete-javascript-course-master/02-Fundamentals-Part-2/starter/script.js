bc8fed817085d099da9161da347b37846c0eea50
DrugJangSu <ljonghyun6088@gmail.com>
2026-02-17 21:43:23 +0900

    Update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,15 @@
 //// Activating Strict Mode
+/*
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio"; <- this will cause an error
+// const private = 534; <- this will also cause an error
+// const if = 23; <- error.
+*/
+//// Functions
--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -692,3 +692,17 @@ console.log(
 
 ///// Section 3 : JavaScript Fundamentals - Part 2
 //// Activating Strict Mode
+/*
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio"; <- this will cause an error
+// const private = 534; <- this will also cause an error
+// const if = 23; <- error.
+*/
+//// Functions
