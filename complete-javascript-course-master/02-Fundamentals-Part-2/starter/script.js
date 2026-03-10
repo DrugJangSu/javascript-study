b90ffda01acbf0e8fbaaccafb47ceb65519a4f35
DrugJangSu <ljonghyun6088@gmail.com>
2026-03-10 15:14:19 +0000

    Update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -13,3 +13,57 @@ if (hasDriversLicense) console.log("I can drive :D");
 // const if = 23; <- error.
 */
 //// Functions
+/*
+"use strict";
+
+function logger() {
+  console.log("My name is Hyun");
+} // this is called the function buddy
+
+// calling / running / invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  // console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+
+/// Assignment[Functions]
+// My Solution
+function describeCountry(country, population, capitalCity) {
+  console.log(country, population, capitalCity);
+  const description = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+  return description;
+}
+const finland = describeCountry("Finland", 6, "Helsinki");
+console.log(finland);
+const southKorea = describeCountry("South Korea", 51, "Seoul");
+console.log(southKorea);
+const japan = describeCountry("Japan", 126, "Tokyo");
+console.log(japan);
+
+// Jonas Solution
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+const descGermany = describeCountry("Germany", 83, "Berlin");
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+
+console.log(descPortugal, descGermany, descFinland);
+*/
+//// Function Declarations vs. Expressions
+"use strict";
