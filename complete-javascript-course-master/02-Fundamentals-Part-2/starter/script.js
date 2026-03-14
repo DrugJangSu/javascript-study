d31f805001cfbad545682747cec3aef1ee28bc41
DrugJangSu <ljonghyun6088@gmail.com>
2026-03-14 18:20:41 +0000

    Update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -297,6 +297,7 @@ checkWinner(scoreDolphins, scoreKoalas);
 */
 
 //// Introduction to Arrays
+/*
 const friend1 = "Michael";
 const friend2 = "Steven";
 const friend3 = "Peter";
@@ -312,3 +313,84 @@ console.log(friends[2]);
 
 console.log(friends.length);
 console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+// friends = ["Bob", "Alice"] <- this will cause an error because we cannot reassign a new array to a constant variable. However, we can change the elements of the array.
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+// console.log(calcAge(years[0])); // this will work because we are passing a single value to the function.
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[2]);
+const age4 = calcAge(years[3]);
+const age5 = calcAge(years[4]);
+console.log(age1, age2, age3, age4, age5);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[2]),
+  calcAge(years[3]),
+  calcAge(years[4]),
+];
+console.log(ages);
+
+/// Assignment[Introduction to Arrays]
+// My solution
+const populations = [1441, 51, 126, 83];
+console.log(populations.length === 4);
+
+function percentageOfWorld1(population) {
+  return (populations[0] / 7900) * 100;
+}
+function percentageOfWorld2(population) {
+  return (populations[1] / 7900) * 100;
+}
+function percentageOfWorld3(population) {
+  return (populations[2] / 7900) * 100;
+}
+function percentageOfWorld4(population) {
+  return (populations[3] / 7900) * 100;
+}
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld2(populations[1]),
+  percentageOfWorld3(populations[2]),
+  percentageOfWorld4(populations[3]),
+];
+
+console.log(percentages);
+
+// second attempt
+const populations = [1441, 51, 126, 83];
+console.log(populations.length === 4);
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+console.log(percentages);
+*/
+//// Basic Array Operations (Methods)
+const friends = ["Michael", "Steven", "Peter"];
+
+friends.push("Jay");
+console.log(friends);
