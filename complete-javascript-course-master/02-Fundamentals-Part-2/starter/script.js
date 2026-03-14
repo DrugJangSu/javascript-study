8c96287f743c6363cb6681b6def4d88bf99aedba
DrugJangSu <ljonghyun6088@gmail.com>
2026-03-14 17:46:21 +0000

    Update

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -295,4 +295,20 @@ const checkWinner = function (avgDolphins, avgKoalas) {
  
 checkWinner(scoreDolphins, scoreKoalas);
 */
+
 //// Introduction to Arrays
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(years);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
