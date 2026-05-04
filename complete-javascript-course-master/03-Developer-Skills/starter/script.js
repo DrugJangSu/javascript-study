6035ba75c92ae6c6ab2d683260b37ad8014a344d
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-04 19:14:47 +0100

    Update

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -7,3 +7,5 @@ const calcAge = birthYear => 2037 - birthYear;
 console.log(calcAge(1991));
 
 //// Installing Node.js and setting up a Dev Environment
+// Using live server extension in VS Code to run the code in the browser
+console.log(calcAge(1991));
