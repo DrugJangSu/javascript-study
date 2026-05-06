d10aa75b3cbb9bc2b5eb73c746a7c138db39bb29
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-06 14:40:25 +0100

    Update

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -14,3 +14,9 @@ console.log(calcAge(1991));
 // Set a specific, measurable, realistic and time based goal
 // Understand the code that you're studying and typing. ALWAYS TYPE THE CODE, no copy and pasting
 // After you learn a new feature or concept, use it immediately
+// Practicing on your own is the most important thing to do. THIS IS NOT OPTIONAL. Without practice outside of courses, you will not learn how to code.
+// Don't be stuck in "tutorial hell"
+// Don't get stuck trying to write the perfect code. Just write tons of code, no matter the quality. Clean and efficient code will come in time.
+// Embrace the fact that you will never know everything. The JavaScript language is huge and is always changing. Focus on learning the fundamentals and then learn new features as you need them.
+// Try not to learn in isolation. Share your goals to make yourself accountable.
+// Courses are only the beginner of your journey, the starting point. You are never going to learn everything in a course. You have to practice on your own and learn new things on your own. The course is just the beginning of your journey, not the end.
