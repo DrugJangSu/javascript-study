475741882ca05b121d1e9e7c3f242912be9c651d
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-06 14:45:23 +0100

    Update

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,5 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/*
 //// Setting up Prettier and VS Code, and installing extensions
 const x = 23;
 if (x === 23) console.log(23);
@@ -9,7 +10,7 @@ console.log(calcAge(1991));
 //// Installing Node.js and setting up a Dev Environment
 // Using live server extension in VS Code to run the code in the browser
 console.log(calcAge(1991));
-
+*/
 //// Learning How To Code
 // Set a specific, measurable, realistic and time based goal
 // Understand the code that you're studying and typing. ALWAYS TYPE THE CODE, no copy and pasting
@@ -21,3 +22,9 @@ console.log(calcAge(1991));
 // Try not to learn in isolation. Share your goals to make yourself accountable.
 // Courses are only the beginner of your journey, the starting point. You are never going to learn everything in a course. You have to practice on your own and learn new things on your own. The course is just the beginning of your journey, not the end.
 // Graph goes like this; EVERYTHING IS AWESOME - > CLIFF OF CONFUSION -> Frustration -> PIT OF DESPAIR -> After a long moment of practice and retries -> Back to awesome -> more competence -> JOB READY
+
+/// To sum up
+// Study courses : Understand code, take callenges and notes
+// Stay motivated! Keep writing lots of code on your own, no matter how bad
+// Learn with other people, devs and begineers, and share progress
+// Keep challenging yourself, run into lots of problems, and fix them
