f4211618013052d7db912844ce720b8572385a40
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-09 14:43:30 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -1872,4 +1872,53 @@ console.log(amplitudeBug);
 // TEST DATA 1 : [17, 21, 23]
 // TEST DATA 2 : [12, 5, -5, 0, 4]
 
-// MY SOLUTION
+// MY SOLUTION ------------------------------------------------
+/*
+const printForecast = function (arr) {
+  // Making a forecast string with the given array of temperatures
+  let forecast = ``;
+  // Make a for loop
+  for (let i = 0; i < arr.length; i++) {
+    // For each element in the array, add a string like "17ºC in 1 day" to the forecast string
+    forecast += `...${arr[i]}ºC in ${i + 1} day${i + 1 > 1 ? `s` : ``} `;
+  }
+  forecast += `...`;
+  console.log(forecast);
+};
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
+*/
+
+// JONAS SOLUTION ------------------------------------------------
+/*
+// 1) Understanding the problem
+// - Array transformed to string, separated by ...
+// -What is the X days? Answer : index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each element to string with ºC
+// - Strings need to contain day (index + 1)
+// - Add ... between elements and start and end of string
+// - Log string to console
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+// If hardcoded...
+// console.log(
+//   `... ${data1[0]}ºC in 1 day ... ${data1[1]}ºC in 2 days ... ${data1[2]}ºC in 3 days ...`,
+// );
+// But if coded properly...
+
+const printForecast = function (arr) {
+  let str = ``;
+  for (let i = 0; i < arr.length; i++) {
+    // str = str + `${arr[i]}ºC`;
+    str += `${arr[i]}ºC in ${i + 1} days ... `;
+  }
+  console.log(`...` + str);
+};
+printForecast(data1);
+printForecast(data2);
+*/
