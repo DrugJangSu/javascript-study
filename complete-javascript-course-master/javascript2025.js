03f9285db0a6c7a945e3254ce35b5350df2f2978
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-20 16:01:10 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -2448,3 +2448,37 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 });
 */
 //// Implementing the Game Logic
+/*
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(`.number`).textContent = secretNumber;
+
+document.querySelector(`.check`).addEventListener(`click`, function () {
+  const guess = Number(document.querySelector(`.guess`).value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(`.message`).textContent = `⛔️No number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct Number!`;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📈Too high!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥You lost the game!`;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(`.message`).textContent = `📉Too low!`;
+      score--;
+      document.querySelector(`.score`).textContent = score;
+    } else {
+      document.querySelector(`.message`).textContent = `💥You lost the game!`;
+    }
+  }
+});
+*/
+//// Manipulating CSS Styles
