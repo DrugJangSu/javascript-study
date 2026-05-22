dde95d4833bf7d5538d4ae78bf18e3d98f293f67
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-22 16:08:26 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -2816,3 +2816,70 @@ document.querySelector(`.again`).addEventListener(`click`, function () {
 /// End of PROJECT #1 : Guess My Number!
 
 //// PROJECT #2 : Modal Window
+/*
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+// console.log(modal);
+// console.log(overlay);
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
+*/
+//// Working With Classes
+/*
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+// console.log(modal);
+// console.log(overlay);
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(`click`, function () {
+    console.log(`Button clicked`);
+    modal.classList.remove(`hidden`);
+    overlay.classList.remove(`hidden`);
+  });
+
+btnCloseModal.addEventListener(`click`, function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+});
+
+overlay.addEventListener(`click`, function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+});
+*/
+/// After DRYing the code;
+/*
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+const openModal = function () {
+  console.log(`Button clicked`);
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+
+btnCloseModal.addEventListener(`click`, closeModal);
+
+overlay.addEventListener(`click`, closeModal);
+
+ */
+//// Handling an "ESC" Keypress Event
