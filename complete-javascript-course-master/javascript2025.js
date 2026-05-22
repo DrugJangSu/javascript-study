a02735cec399862235717e11af3797c12ddf2726
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-22 21:22:54 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -2883,3 +2883,40 @@ overlay.addEventListener(`click`, closeModal);
 
  */
 //// Handling an "ESC" Keypress Event
+/*
+'use strict';
+
+const modal = document.querySelector(`.modal`);
+const overlay = document.querySelector(`.overlay`);
+const btnCloseModal = document.querySelector(`.close-modal`);
+const btnsOpenModal = document.querySelectorAll(`.show-modal`);
+
+const openModal = function () {
+  console.log(`Button clicked`);
+  modal.classList.remove(`hidden`);
+  overlay.classList.remove(`hidden`);
+};
+
+const closeModal = function () {
+  modal.classList.add(`hidden`);
+  overlay.classList.add(`hidden`);
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener(`click`, openModal);
+
+btnCloseModal.addEventListener(`click`, closeModal);
+
+overlay.addEventListener(`click`, closeModal);
+
+document.addEventListener(`keydown`, function (e) {
+  console.log(e.key);
+
+  if (e.key === `Escape` && !modal.classList.contains(`hidden`)) {
+    closeModal();
+  }
+});
+*/
+
+//// End of PROJECT #2 : Modal Window
+
+//// PROJECT #3 : PIG GAME
