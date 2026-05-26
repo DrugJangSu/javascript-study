bad3197ba2a9b9432300b4c545f1957ad4e2beee
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-26 17:50:35 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -2920,3 +2920,16 @@ document.addEventListener(`keydown`, function (e) {
 //// End of PROJECT #2 : Modal Window
 
 //// PROJECT #3 : PIG GAME
+/*
+// Selecting elements
+const score0El = document.querySelector(`#score--0`);
+const score1El = document.getElementById(`score--1`);
+// These are the exactly the same, but the first one is more recommended because it is more flexible and can be used in other situations, while the second one is only for selecting elements by ID.
+const diceEl = document.querySelector(`.dice`);
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add(`hidden`);
+*/
+//// Rolling the Dice
