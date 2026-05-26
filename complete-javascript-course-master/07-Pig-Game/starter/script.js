c1bcb764ba2c310e858893e2c950a02538d3c541
DrugJangSu <ljonghyun6088@gmail.com>
2026-05-26 17:00:09 +0100

    Update

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector(`#score--0`);
+const score1El = document.getElementById(`score--1`);
+// These are the exactly the same, but the first one is more recommended because it is more flexible and can be used in other situations, while the second one is only for selecting elements by ID.
+const diceEl = document.querySelector(`.dice`);
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add(`hidden`);
