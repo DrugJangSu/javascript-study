bdb3779f4294b1a9077d52399e5e1ca28390048a
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-09 18:18:11 +0100

    Update

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -3,16 +3,27 @@
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
-    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = `Steven`; // This will work since Javascript tries to look for the variable name first in the same scope block. And this will priotise this one since it's in the same scope. (so if this code is active the console will print out Steven instead of Jonas)
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      // Reassigning outer scope's variable
+      //  output = `NEW OUTPUT!`; // This manipulates the existing variable inside of the child scope(inner scope) However if this should be possible the same variable need to be defined not as const(like the top).
+      const output = `NEW OUTPUT!`; // However because we defined the new variable with the same name, this will not affect the previous output anymore thus printing out the original output.
     }
     // console.log(str); // This wouldn't work, because const and let variables are block scoped which means they're only available inside the block.
     console.log(millenial); // This works since it's a var (var variables are function scoped, so they ignore the block.)
+    // console.log(add(2, 3)); // This wouldn't work because the code's currently in "strict mode", in which makes the function block scoped. if I turn off strict mode it will work.
+    console.log(output); //
   }
   printAge();
 
@@ -23,3 +34,5 @@ const firstName = `Jonas`;
 calcAge(1991);
 // console.log(age); // This wouldn't work since the scope chain is a one way street, so only the inner scope can have access to the outer scope, not the other way around.
 // printAge(); // The same as this one.
+
+// This is the reason why it's okay to have many functions with the exact same parameter names, in the same way it's not a problem to have functions wit the same variable names inside of them.
