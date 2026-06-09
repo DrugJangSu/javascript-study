cdb152e579a53b83a52f225360a5251dfd05fc86
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-09 18:04:14 +0100

    Update

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -3,8 +3,16 @@
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    const output = `${firstName}, You are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str); // This wouldn't work, because const and let variables are block scoped which means they're only available inside the block.
+    console.log(millenial); // This works since it's a var (var variables are function scoped, so they ignore the block.)
   }
   printAge();
 
@@ -14,3 +22,4 @@ function calcAge(birthYear) {
 const firstName = `Jonas`;
 calcAge(1991);
 // console.log(age); // This wouldn't work since the scope chain is a one way street, so only the inner scope can have access to the outer scope, not the other way around.
+// printAge(); // The same as this one.
