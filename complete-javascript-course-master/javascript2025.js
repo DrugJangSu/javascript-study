9064e5015dc9ee140d0f74c2d7aa0e03cfa9b2ce
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-16 17:41:00 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -3631,3 +3631,13 @@ console.log(z === window.z);
 */
 
 //// The this Keyword ---------------------------------------
+/// How the this Keyword works
+// this keyword/variable : Special variable that is created for every execution context (every function). Takes the value of (points to) the "owner" of the function in which the this keyword is used.
+// "this" is Not static. It depends on how the function is called, and its value is only assigned when the function is actually called.
+// - Method 👉 this = <Object that is calling the method>
+// - Simple function call 👉 this = undefined (In strict mode! Otherwise : window (in the browser))
+// - Arrow functions 👉 this = <this of surrounding function (lexical this)>
+// - Event listener 👉 this = <DOM element that the handler is attached to>
+// - new, call, apply, bind 👉 <later in the course>
+
+// <this does NOT point to the function itself, but also NOT the its variable environment!>
