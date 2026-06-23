47ce5edb690384c796e5eb9eb670199a7084fc0e
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-23 20:17:19 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -3946,3 +3946,18 @@ console.log(`Clone`, jessicaClone);
 ///// END OF SECTION 8 --------------------------------------------------------------------------------------------
 
 ///// Section 9 : Data Structures, Modern Operators and Strings --------------------------------------------------------------------------------------------------------
+//// Destructuring Arrays
+`use strict`;
+
+const restaurant = {
+  name: `Classico Italiano`,
+  location: `Via Angelo Tavanti 23, Firenze, Italy`,
+  categories: [`Italian`, `Pizzeria`, `Vegetarian`, `Organic`],
+  starterMenu: [`Focaccia`, `Bruschetta`, `Garlic Bread`, `Caprese Salad`],
+  mainMenu: [`Pizza`, `Pasta`, `Risotto`],
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const a = arr[0];
+const a = arr[0];
