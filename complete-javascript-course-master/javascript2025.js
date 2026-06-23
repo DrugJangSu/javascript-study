82ef73c45deda64c7a37bf2c0f8bd8516d321455
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-23 21:34:56 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -4012,6 +4012,7 @@ console.log(p, q, r);
 
 */
 /// ASSIGNMENT[Destructuring Arrays]
+/*
 const books = [
   {
     title: "Algorithms",
@@ -4237,4 +4238,28 @@ const books = [
   },
 ];
 
+/// 1.1 Destructure the books array into two variables called `firstBook` and `secondBook`
+// Answer
+const [firstBook, secondBook] = books;
+// console.log(firstBook, secondBook);
+
+/// 1.2 Destructure the books array into a variable called thirdBook. You must skip the first two books.
+// Answer
+const [, , thirdBook] = books;
+console.log(thirdBook);
+
+/// 1.3 Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount. In the result of your destructuring, the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+const ratings = [
+  ["rating", 4.19],
+  ["ratingsCount", 144584],
+];
+// Answer
+const [[, rating], [, ratingsCount]] = ratings;
+
+/// 1.4 Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+const ratingStars = [63405, 1808];
+// Answer
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+console.log(fiveStarRatings, oneStarRatings, threeStarRatings);
+*/
 //// Destructuring Objects --------------------------------------
