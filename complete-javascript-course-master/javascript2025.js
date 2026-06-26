c3b8e61ec71f749ea61a0f64ae1d19b53bca8632
DrugJangSu <ljonghyun6088@gmail.com>
2026-06-26 16:37:50 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -4344,7 +4344,7 @@ const {
 console.log(o, c);
 */
 
-/// ASSIGNMENT[Destructuring Objects]
+/// ASSIGNMENT[Destructuring Objects] --------------------
 const books = [
   {
     title: "Algorithms",
@@ -4570,29 +4570,35 @@ const books = [
   },
 ];
 /// 2.1 Destructure the first book object from the books array into variables called title, author and ISBN.
+/// My Answer
 
 //
 //
 /// 2.2 Each book object has the keywords property. Destructure the first book object from the books array into a variable called tags. The tags variable should be assigned with the value of the keywords property.
+/// My Answer
 
 //
 //
 /// 2.3 The seventh book from the books array is missing the programmingLanguage property. Destructure the seventh book object (books[6]) into variables called language and programmingLanguage. Assign the programmingLanguage variable with a default value of 'unknown'.
+/// My Answer
 
 //
 //
 /// 2.4 Below are two variables called bookTitle and bookAuthor. Reassign them with the values of the title and author properties of the first book object from the books array.
 let bookTitle = "unknown";
 let bookAuthor = "unknown";
+/// My Answer
 
 //
 //
 /// 2.5 Each book object has a deeply nested rating property as illustrated. Destructure the first book object from the books array into a variable called bookRating. In the result of your destructuring, the bookRating variable should be assigned with the value of the book[0].thirdParty.goodreads.rating property.
 /// Please do most of the work on the left side of the assignment operator: const ... = books[0];
+/// My Answer
 
 //
 //
 /// 2.6 Write a function called printBookInfo that has three parameters called title, author and year. This function should work for a single object passed as an argument, and it should log to the console information about the book in this format: "${title} by ${author}, ${year}".
+/// My Answer
 
 //
 //
