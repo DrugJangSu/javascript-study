fb799858283354d1ba2edca321c5828a5f912936
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-14 17:29:33 +0100

    Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -65,6 +65,11 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`,
     );
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 restaurant.orderDelivery({
@@ -93,7 +98,7 @@ const {
 // Default values
 // const { menu = [], starterMenu: starters = [] } = restaurant;
 // console.log(menu, starters);
-
+/*
 // Mutating variables
 let a = 111;
 let b = 999;
@@ -109,7 +114,7 @@ const {
 // console.log(o, c);
 
 //
-
+*/
 /////////////////////////////////////////////////////////
 //// Destructuring Arrays
 /*
@@ -435,7 +440,7 @@ function printBookInfo(title, author, year) {
 //
 //
 */
-
+/*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);
@@ -485,3 +490,39 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = `Ristorante Roma`;
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+*/
+//
+//// Rest Pattern and Parameters -------------------
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+/// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+/// 2) Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza(`mushrooms`, `onion`, `olives`, `spinach`);
+restaurant.orderPizza(`mushrooms`);
