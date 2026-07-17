a51732a1b009135606f15cd655baeda9e503a4c9
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-17 21:30:27 +0100

    Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -558,3 +558,16 @@ if (restaurant.orderPizza) {
 
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 */
+
+//// The Nullish Coalescing Operator --------------------
+/*
+// restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish : null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+*/
+
+//// Logical Assignment Operators -----------------------
