142a0eb9e8712fd669afb3b462ecff1ee122676f
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-21 17:38:41 +0100

    Update

--- complete-javascript-course-master/javascript2025.js
+++ complete-javascript-course-master/javascript2025.js
@@ -5874,9 +5874,16 @@ console.log(rest1, rest2);
 
 /// 7.1 - Some of the book objects from the books array are missing the edition property. Loop over the books array, and assign this property with a number 1 (if it doesn't already exist). Use logical assignment operators.
 // My Answer:
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ||= 1;
+}
 
 /// 7.2 - 7.2﻿ Some of the book objects from the books array have the highlighted property, which by default is set to true. Iterate over the books array, and if the thirdParty.goodreads.rating property is less than 4.2, reassign it with false.
 /// Use the &&= operator (tip: you may also need the ! operator)
 // My Answer:
-
+for (let i = 0; i < books.length; i++) {
+  books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2)
+}
 */
+
+//// CHALLENGE #1 -----------------------
