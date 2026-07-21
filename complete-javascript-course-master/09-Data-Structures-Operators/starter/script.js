bbfa92dbc0921c84c8d1cbf220a26592dbe52d7e
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-21 17:13:59 +0100

    Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -573,7 +573,8 @@ console.log(guestCorrect);
 //// Logical Assignment Operators -----------------------
 const rest1 = {
   name: `Capri`,
-  numGuests: 20,
+  // numGuests: 20,
+  numGuests: 0,
 };
 
 const rest2 = {
@@ -585,9 +586,13 @@ const rest2 = {
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
 
-rest1.numGuests ||= 10;
-rest2.numGuests ||= 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
 
 // These two have the same effect as the above code
+
+/// Nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
 
 console.log(rest1, rest2);
