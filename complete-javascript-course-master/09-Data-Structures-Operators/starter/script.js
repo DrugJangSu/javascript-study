a3a51838b05d6adb2d0a4214daca7426c123676f
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-23 12:20:19 +0100

    Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -571,7 +571,7 @@ console.log(guestCorrect);
 */
 
 //// Logical Assignment Operators -----------------------
-
+/*
 const rest1 = {
   name: `Capri`,
   // numGuests: 20,
@@ -604,3 +604,29 @@ rest1.owner &&= `<ANONYMOUS>`;
 rest2.owner &&= `<ANONYMOUS>`;
 
 console.log(rest1, rest2);
+*/
+
+//// CHALLENGE #1 -----------------------------------------------
+
+/*
+We're building a football betting app (soccer for my American friends 😅)! Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with that data.
+
+Here are the tasks;
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, along with the number of goals that were scored in total
+
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+
+
+TEST DATA FOR 6 : Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+*/
