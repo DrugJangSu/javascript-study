ef590101562475642182ea4bc21d547096de8107
DrugJangSu <ljonghyun6088@gmail.com>
2026-07-23 12:25:04 +0100

    Update

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -630,3 +630,43 @@ Here are the tasks;
 TEST DATA FOR 6 : Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
 
 */
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borussia Dortmund',
+  players: [
+    [
+      `Neuer`,
+      `Pavard`,
+      `Martinez`,
+      `Alaba`,
+      `Davies`,
+      `Kimmich`,
+      `Goretzka`,
+      `Coman`,
+      `Muller`,
+      `Gnarby`,
+      `Lewandowski`,
+    ],
+    [
+      `Burki`,
+      `Schulz`,
+      `Hummels`,
+      `Akanji`,
+      `Hakimi`,
+      `Weigl`,
+      `Witsel`,
+      `Hazard`,
+      `Brandt`,
+      `Sancho`,
+      `Gotze`,
+    ],
+  ],
+  score: '4:0',
+  scored: [`Lewandowski`, `Gnarby`, `Lewandowski`, `Hummels`],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
